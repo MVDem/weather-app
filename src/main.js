ec158185748b732962136d5ad9c5362dac5686c5
AlexeyT74 <alexey.tal@gmail.com>
2024-04-11 21:49:04 +0300

    Full functional queries with sending requests to Accuweather service

--- src/main.js
+++ src/main.js
@@ -1,12 +1,42 @@
-import {} from './http/client';
-import './style.css';
-import { get5DayForecast, getCityKey, getCurrentWeather, getLocationCity, searchCity } from './http/client.js';
+import "./style.css";
+import {
+  get5DayForecast,
+  getCityKey,
+  getCurrentWeather,
+  getLocationCity,
+  searchCity,
+} from "./http/client.js";
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
-export async function init() {
-    const currCity = await getLocationCity();
-    getCurrentWeather(currCity.key).then((resolve) => {
-        console.log(`Current weather in ${currCity.name} is: ${resolve[0].WeatherText}`);    
+export function init() {
+
+}
+
+// this function should be removed when "client.test.js" is finished
+function testClient(){
+     getLocationCity().then((city) => {
+      console.log(city);
+      getCurrentWeather(city.key).then((resolve) => {
+        console.log(
+          `Current weather in ${city.name} is: ${resolve[0].WeatherText}`
+        );
+      });
+    });
+
+  searchCity("Tel").then((cityArr) => {
+    console.log(cityArr);
+  });
+
+    getCityKey("Tel Aviv").then((cityKey) =>{
+        console.log(cityKey);
+    })
+
+    getCurrentWeather("215854").then((weather)=>{
+        console.log(weather);
+    })
+
+    get5DayForecast("215854").then((forecast)=>{
+        console.log(forecast);
     })
 }
