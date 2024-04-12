020dfe47b446eabb2481342aade06cc978ed3037
Maxim <maxpodolsky.dev@gmail.com>
2024-04-12 18:40:12 +0300

    disabled photo http request

--- src/http/unsplash.js
+++ src/http/unsplash.js
@@ -7,8 +7,8 @@ const axiosInstance = axios.create({
 
 export async function getPhoto(name) {
   try {
-    return (await axiosInstance.get(`&query=${name}`)).data.results[0].urls
-      .full;
+    //return (await axiosInstance.get(`&query=${name}`)).data.results[0].urls.full;
+    return '../public/default-background.jpg';
   } catch (error) {
     console.error('Error while fetching city photo', error);
     return '../public/default-background.jpg';
