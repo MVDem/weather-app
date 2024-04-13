d5f51a0d7c7b4bd0e722e0810910984848c32dda
AlexeyT74 <alexey.tal@gmail.com>
2024-04-13 22:01:39 +0300

    Test 3

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,5 @@
 import { defineConfig } from 'vite';
-// import { resolve } from 'path'
 
 export default defineConfig({
-  base: 'weather-app',
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, 'index.html'),        
-  //     },
-  //   },
-  // },
+  base: '/weather-app/',
 });
