91bfffb43e0a2330c028422b3bb97337e8c8c8bc
Valeriia Melnikov <valeriia.melnikov@gmail.com>
2024-04-14 00:13:59 +0300

    feat: update unsplush request, add event to favorites

--- src/http/unsplash.js
+++ src/http/unsplash.js
@@ -2,13 +2,17 @@ import { UNSPLASH_API_KEY } from '../config';
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: `https://api.unsplash.com/search/photos/?client_id=${UNSPLASH_API_KEY}&page=1`,
+  baseURL: `https://api.unsplash.com/photos/`,
 });
 
-export async function getPhoto(name) {
+export async function getPhoto(name, theme) {
+  const themeForRequest = theme === 'dark' ? 'evening' : 'morning';
   try {
-    //return (await axiosInstance.get(`&query=${name}`)).data.results[0].urls.full;
-    return '../public/default-background.jpg';
+    return (
+      await axiosInstance.get(
+        `random?orientation=landscape&query=${name}&query=${themeForRequest}&client_id=${UNSPLASH_API_KEY}`
+      )
+    ).data.urls.regular;
   } catch (error) {
     console.error('Error while fetching city photo', error);
     return '../public/default-background.jpg';
--- src/main.js
+++ src/main.js
@@ -10,34 +10,29 @@ import {
   manageFavorite,
   renderFavorites,
 } from './utils.js';
-import axiosInstance from './http/index.js';
 
 const state = {
-  favorite: [],
+  favorite: [
+    'Tel Aviv',
+    'Nes Ziyyona',
+    // { LocalizedName: 'Tel Aviv', Key: '215854' },
+    // { LocalizedName: 'Jerusalem', Key: '213225' },
+    // { LocalizedName: 'Nes Ziyyona', Key: '212560' },
+  ],
   currentCityIndex: 0,
+  theme: 'dark',
+  units: 'C',
+  currCity: '',
 };
 
 document.addEventListener('DOMContentLoaded', init);
 
 export async function init() {
   const currCity = await getLocationCity();
-  renderCity(currCity);
-
-  if (state.favorite.length) {
-    const prevBtnEl = document.querySelector('#prev-page');
-    prevBtnEl.addEventListener('click', () => {
-      console.log('render prev city from state');
-      renderCity(state.favorite[state.currentCityIndex - 1]);
-    });
-
-    const nextBtnEl = document.querySelector('#next-page');
-    nextBtnEl.classList.remove('disabled');
-    nextBtnEl.addEventListener('click', () => {
-      prevBtnEl.classList.remove('disabled');
-      console.log('render next city from state');
-      //render next city from state
-    });
-  }
+  displayCity(currCity);
+  renderFavorites(state.favorite);
+  // navigateFavorites();
+  chooseFavorite();
 }
 
 const burgerEl = document.querySelector('#burger');
@@ -57,8 +52,12 @@ searchFormEl.addEventListener('submit', async (event) => {
   const data = new FormData(event.target);
   const cityName = data.get('city');
   try {
-    const [cityData] = await searchCity(cityName);
-    renderCity(cityData);
+    const cityData = await searchCity(cityName);
+
+    if (cityData === null || !cityData.length) throw new Error('City was not found');
+    state.currCity = cityData[0].LocalizedName;
+
+    displayCity(cityData[0]);
   } catch (error) {
     showFailMessage(error.message);
   }
@@ -67,32 +66,31 @@ searchFormEl.addEventListener('submit', async (event) => {
 const unitsRadioBtn = document.querySelector('#header-units-radioBtn');
 unitsRadioBtn.addEventListener('change', (event) => {});
 
-function renderCity(city) {
-  const isFavorite = state.favorite.includes(city);
+function displayCity(cityObj) {
+  const isFavorite = state.favorite.includes(cityObj.LocalizedName);
   try {
-    renderCityPhoto(city.LocalizedName);
+    renderCityPhoto(cityObj.LocalizedName, state.theme);
 
-    renderCityName(city.LocalizedName, isFavorite);
+    renderCityName(cityObj.LocalizedName, isFavorite);
 
-    getCurrentWeather(city.Key, true).then((resolve) => {
+    getCurrentWeather(cityObj.Key, true).then((resolve) => {
       renderWeather(resolve[0]);
     });
 
-    get5DayForecast(city.Key, true).then((data) => {
+    get5DayForecast(cityObj.Key, true).then((data) => {
       render5DaysWeather(data.DailyForecasts);
     });
-    changeFavoriteCity();
+    toogleFavoriteCity();
   } catch (error) {
-    showFailMessage(error.message || 'Something weht wrong');
+    showFailMessage(error.message || 'Something went wrong');
   }
 }
 
-function changeFavoriteCity() {
+function toogleFavoriteCity() {
   const bookmarkEl = document.querySelector('#bookmark');
 
-  bookmarkEl?.addEventListener('click', (event) => {
+  bookmarkEl.addEventListener('click', (event) => {
     const cityName = document.querySelector('#bookmark-city-name').textContent;
-
     const buttonEl = event.currentTarget;
     if (buttonEl.classList.contains('favorite')) {
       buttonEl.classList.remove('favorite');
@@ -102,5 +100,54 @@ function changeFavoriteCity() {
       manageFavorite(state.favorite, cityName, STATE_OPERATION.add);
     }
     renderFavorites(state.favorite);
+    chooseFavorite();
+    // navigateFavorites();
   });
 }
+
+function chooseFavorite() {
+  const favorites = document.querySelectorAll('.header__dropdown > ul > li');
+
+  favorites.forEach((el) => {
+    el.addEventListener('click', async (e) => {
+      console.log(e);
+      const cityData = await searchCity(el.textContent);
+      console.log('cityData: ', cityData);
+
+      if (cityData === null || !cityData.length) throw new Error('City was not found');
+      state.currCity = cityData[0].LocalizedName;
+
+      displayCity(cityData[0]);
+    });
+  });
+}
+
+// function navigateFavorites() {
+//   if (state.favorite.length < 1) {
+//     nextBtnEl.classList.add('disabled');
+//     prevBtnEl.classList.add('disabled');
+//     return;
+//   }
+
+//   const indexCurrentCity = state.favorite.indexOf(state.currCity);
+
+//   state.currentCityIndex = indexCurrentCity >= 0 ? indexCurrentObj : 0;
+
+//   const prevBtnEl = document.querySelector('#prev-page');
+//   prevBtnEl.classList.remove('disabled');
+//   prevBtnEl.addEventListener('click', () => {
+//     if (state.currentCityIndex === 0) state.currentCityIndex = state.favorite.length - 1;
+//     else state.currentCityIndex -= 1;
+//     console.log('currentCityIndex: prev ', state.currentCityIndex);
+//     displayCity(state.favorite[state.currentCityIndex]);
+//   });
+
+//   const nextBtnEl = document.querySelector('#next-page');
+//   nextBtnEl.classList.remove('disabled');
+//   nextBtnEl.addEventListener('click', () => {
+//     if (state.currentCityIndex === state.favorite.length - 1) state.currentCityIndex = 0;
+//     else state.currentCityIndex += 1;
+//     console.log('currentCityIndex: next', state.currentCityIndex);
+//     displayCity(state.favorite[state.currentCityIndex]);
+//   });
+// }
